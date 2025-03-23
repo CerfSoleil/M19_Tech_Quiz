c2d1ed92617d7f5dc15a33ed9500dea3a66e1080
CerfSoleil <robedj12@gmail.com>
2025-03-23 09:45:42 -0500

    Component tests completed

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -15,4 +15,32 @@ describe('<Quiz />', () => {
         cy.contains('Start Quiz').click()
         cy.get('h2').invoke('text').should('match', /\?/)
     });
+
+    it('presents another question when the last is answered', () => {
+        cy.mount(<Quiz />)
+        cy.contains('Start Quiz').click()
+        cy.get('button').first().click()
+        cy.get('h2').invoke('text').should('match', /\?/)
+    });
+
+    it('ends after 10 questions are answered and display score', () => {
+        cy.mount(<Quiz />)
+        cy.contains('Start Quiz').click()
+        Cypress._.times(10, (_i: number) => {
+            cy.get('button').first().click();
+        });
+        cy.get('h2').should('have.text', 'Quiz Completed')
+        cy.get('.alert-success').contains('Your score:').should('be.visible')
+    });
+
+    it('clicking button after quiz ends should start a new quiz', () => {
+        cy.mount(<Quiz />)
+        cy.contains('Start Quiz').click()
+        Cypress._.times(10, (_i: number) => {
+            cy.get('button').first().click();
+        });
+        cy.get('button').contains('Take New Quiz').should('be.visible');
+        cy.contains('Take New Quiz').click()
+        cy.get('h2').invoke('text').should('match', /\?/)
+    });
 });
